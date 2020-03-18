28938e6526016fa8e11bf4f1704f5e33e8e88bf7
Colton Thompson <thompson.coltonj@gmail.com>
2020-03-17 23:23:45 -0400

    Create preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+plt.style.use("ggplot")
+
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout
+
+# Loading/Reading in the Data
+df = pd.read_csv("BTC-USD.csv")
+
+# Data Preprocessing
+### Setting the datetime index as the date, only selecting the 'Close' column, then only the last 1000 closing prices.
+df = df.set_index("Date")[['Close']].tail(1000)
+df = df.set_index(pd.to_datetime(df.index))
+
+# Normalizing/Scaling the Data
+scaler = MinMaxScaler()
+df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns, index=df.index)
