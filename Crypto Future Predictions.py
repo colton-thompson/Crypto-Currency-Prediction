2ffda975f9628d2efee02bf590a48acc0c0441a6
Colton Thompson <thompson.coltonj@gmail.com>
2020-03-18 02:18:10 -0400

    it's alive!!!

---
+++ Crypto Future Predictions.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Recurrent Neural Network
+
+# ### Importing Libraries
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+plt.style.use("ggplot")
+
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout
+
+
+# ### Data Loading
+
+# In[2]:
+
+
+df = pd.read_csv("BTC-USD.csv")
+
+
+# ## Data Preprocess
+
+# In[3]:
+
+
+#  Setting the datetime index as the date, only selecting the 'Close' column, then only the last 1000 closing prices.
+df = df.set_index("Date")[['Close']].tail(1000)
+df = df.set_index(pd.to_datetime(df.index))
+
+# Normalizing/Scaling the Data
+scaler = MinMaxScaler()
+df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns, index=df.index)
+
+
+# In[4]:
+
+
+# Checking the newly formatted DataFrame
+df.head()
+
+
+# In[5]:
+
+
+# Plotting the Closing Prices
+df.plot(figsize=(14,8))
+plt.title("BTC Closing Prices")
+plt.ylabel("Price (Normalized)")
+plt.show()
+
+
+# ## Functions to prepare the data for LTSM
+
+# In[6]:
+
+
+def split_sequence(seq, n_steps_in, n_steps_out):
+    """
+    Splits the univariate time sequence
+    """
+    X, y = [], []
+    
+    for i in range(len(seq)):
+        end = i + n_steps_in
+        out_end = end + n_steps_out
+        
+        if out_end > len(seq):
+            break
+        
+        seq_x, seq_y = seq[i:end], seq[end:out_end]
+        
+        X.append(seq_x)
+        y.append(seq_y)
+    
+    return np.array(X), np.array(y)
+
+
+# In[7]:
+
+
+def visualize_training_results(results):
+    """
+    Plots the loss and accuracy for the training and testing data
+    """
+    history = results.history
+    plt.figure(figsize=(12,4))
+    plt.plot(history['val_loss'])
+    plt.plot(history['loss'])
+    plt.legend(['val_loss', 'loss'])
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.show()
+    
+    plt.figure(figsize=(12,4))
+    plt.plot(history['val_accuracy'])
+    plt.plot(history['accuracy'])
+    plt.legend(['val_accuracy', 'accuracy'])
+    plt.title('Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.show()
+
+
+# In[8]:
+
+
+def layer_maker(n_layers, n_nodes, activation, drop=None, d_rate=.5):
+    """
+    Creates a specified number of hidden layers for an RNN
+    Optional: Adds regularization option - the dropout layer to prevent potential overfitting (if necessary)
+    """
+    
+    # Creating the specified number of hidden layers with the specified number of nodes
+    for x in range(1,n_layers+1):
+        model.add(LSTM(n_nodes, activation=activation, return_sequences=True))
+
+        # Adds a Dropout layer after every Nth hidden layer (the 'drop' variable)
+        try:
+            if x % drop == 0:
+                model.add(Dropout(d_rate))
+        except:
+            pass
+
+
+# ## Splitting the Data
+
+# In[9]:
+
+
+# How many periods looking back to learn
+n_per_in  = 30
+
+# How many periods to predict
+n_per_out = 10
+
+# Features (in this case it's 1 because there is only one feature: price)
+n_features = 1
+
+# Splitting the data into appropriate sequences
+X, y = split_sequence(list(df.Close), n_per_in, n_per_out)
+
+# Reshaping the X variable from 2D to 3D
+X = X.reshape((X.shape[0], X.shape[1], n_features))
+
+
+# ## Modeling - LSTM (RNN)
+# 
+# ### Creating the Neural Network
+
+# In[10]:
+
+
+# Instatiating the model
+model = Sequential()
+
+# Activation
+activ = "softsign"
+
+# Input layer
+model.add(LSTM(30, activation=activ, return_sequences=True, input_shape=(n_per_in, n_features)))
+
+# Hidden layers
+layer_maker(n_layers=6, n_nodes=12, activation=activ)
+
+# Final Hidden layer
+model.add(LSTM(10, activation=activ))
+
+# Output layer
+model.add(Dense(n_per_out))
+
+# Model summary
+model.summary()
+
+
+# In[11]:
+
+
+# Compiling the data with selected specifications
+model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+
+
+# ### Fitting and Training the RNN
+
+# In[12]:
+
+
+res = model.fit(X, y, epochs=800, batch_size=32, validation_split=0.1)
+
+
+# ### Plotting the Accuracy and Loss
+
+# In[13]:
+
+
+visualize_training_results(res)
+
+
+# ## Visualizing the Predictions
+# 
+# ### Validating the Model
+# 
+# Plotting the difference between Actual closing prices and the Predicted prices.
+
+# In[14]:
+
+
+plt.figure(figsize=(12,5))
+
+# Getting predictions by predicting from the last available X variable
+yhat = model.predict(X[-1].reshape(1, n_per_in, n_features)).tolist()[0]
+
+# Transforming values back to their normal prices
+yhat = scaler.inverse_transform(np.array(yhat).reshape(-1,1)).tolist()
+
+# Getting the actual values from the last available y variable which correspond to its respective X variable
+actual = scaler.inverse_transform(y[-1].reshape(-1,1))
+
+# Printing and plotting those predictions
+print("Predicted Prices:\n", yhat)
+plt.plot(yhat, label='Predicted')
+
+# Printing and plotting the actual values
+print("\nActual Prices:\n", actual.tolist())
+plt.plot(actual.tolist(), label='Actual')
+
+plt.title(f"Predicted vs Actual Closing Prices")
+plt.ylabel("Price")
+plt.legend()
+plt.savefig("BTC_validation.png")
+plt.show()
+
+
+# ### Predicting/Forecasting the future prices
+
+# In[17]:
+
+
+# Predicting off of y because it contains the most recent dates
+yhat = model.predict(np.array(df.tail(n_per_in)).reshape(1, n_per_in, n_features)).tolist()[0]
+
+# Transforming the predicted values back to their original prices
+yhat = scaler.inverse_transform(np.array(yhat).reshape(-1,1)).tolist()
+
+# Creating a DF of the predicted prices
+preds = pd.DataFrame(yhat, index=pd.date_range(start=df.index[-1], periods=len(yhat), freq="D"), columns=df.columns)
+
+# Printing the predicted prices
+print(preds)
+
+# Number of periods back to visualize the actual values
+pers = 20
+
+# Transforming the actual values to their original price
+actual = pd.DataFrame(scaler.inverse_transform(df[["Close"]].tail(pers)), index=df.Close.tail(pers).index, columns=df.columns).append(preds.head(1))
+
+# Plotting
+plt.figure(figsize=(16,6))
+plt.plot(actual, label="Actual Prices")
+plt.plot(preds, label="Predicted Prices")
+plt.ylabel("Price")
+plt.xlabel("Dates")
+plt.title(f"Forecasting the next {len(yhat)} days")
+plt.legend()
+plt.savefig("BTC_predictions.png")
+plt.show()
+
+
+# In[ ]:
+
+
+
+
